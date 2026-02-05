9048c39d459b4877158e26b04f58b8cb78fb9a81
Paulof-pc <paulof-pc@codecadet-paulof.Home>
2026-02-05 12:11:58 +0000

    Fixed minor css issues in smaller screens; Added Developer/Tech skills sections to home page

--- js/views/homeView.js
+++ js/views/homeView.js
@@ -2,16 +2,36 @@ export default function homeView() {
   return `
     <section class="home-page">
       
-    <h2>Welcome</h2>
+      <h2>Welcome</h2>
 
-    <p>This is the home page of Paulo Ferreira.</p> 
-    <p>My ever first SPA (Single Page App)</p>
+      <p>This is the home page of Paulo Ferreira.</p> 
+      <p>My ever first SPA (Single Page App)</p>
 
-    <br>
+      <br>
 
       <div class="about-photo">
         <img src="https://ca.slack-edge.com/T0J8THHCZ-U09F69Q2R99-5073dc0c9af7-512" alt="Paulo Ferreira" />
       </div>
+
+      <br>
+
+      <p> Junior Software Developer with hands-on experience building Web Applications and other projects based on Spring Boot, where the communication with the Java backend was made via REST APIs.<br> 
+      I've integrated web applications with PostgreSQL databases deployed in AWS, which is the cloud provider I had contact with. First of many, I hope.<br>
+      I've developed frontend interfaces based on JavaScript, HTML and CSS, which gives me the foundations to learn all state-of-the-art frameworks.<br>
+      I've always worked in team-based and time-constrained environments and now I am ready to take it to another   level, developing software solutions.</p>
+
+      <h3>Technical Skills</h3>
+
+      <ul class="backend-skills">
+        <li><img src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white" alt="Java" /></li>
+        <li><img src="https://img.shields.io/badge/SpringBoot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white" alt="Spring Boot" /></li>
+        <li><img src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="PostgreSQL" /></li>
+      </ul>
+      <br>
+      <ul class="frontend-skills">
+        <li><img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript" /></li>
+        <li><img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" /></li>
+        <li><img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="CSS3" /></li>
 
     </section>
   `;
